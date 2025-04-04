320044bac122c4425f5055675311b8f0c955fddd
Tomás Real <tomasreal.art3d@gmail.com>
2025-04-04 14:23:26 +0100

    Add MyRPG program

---
+++ MyRPG/Player.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+
+
+    public class Player
+    {
+        private int xp;
+        private float health;
+
+        public string Name {get;}
+        private float maxHealth;
+
+        public float MaxHealth{
+            get{
+                return maxHealth = 100+(Level-1)*20;
+            }
+        }
+        public int Level
+        {
+            get{
+                return 1+XP/1000;
+            }
+        }
+        public float Health
+        {
+            get{
+                return health;
+            }
+            set{
+
+                if(value<0)
+                    health = 0;
+                else if (value > MaxHealth)
+                        health = MaxHealth;
+                else
+                    health = value;
+            }
+        }
+        public int XP
+        {
+            get{
+                return xp;
+            }
+            set{
+                xp += value;
+            }
+        }
+        public Player(string name)
+        {
+            Name = name;
+            health = MaxHealth;
+            xp = 0;
+        }
+
+
+        public void TakeDamage(float damage)
+        {
+            Health = health-damage;
+            XP = Convert.ToInt32(damage/20);
+        }
+
+    }
+
